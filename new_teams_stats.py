e37c5ce9216ef6979c43318756c13458bd111a38
Christopher Thompson <chth2116@gmail.com>
2021-08-18 16:35:30 -0600

    test

--- new_teams_stats.py
+++ new_teams_stats.py
@@ -23,7 +23,7 @@ def update_team_list():
         }
 
     teams = requests.request("GET", url, headers=headers, params=querystring).text
-    print(teams)
+    print(teams['response'])
     f.write(teams)
     f.close()
 
