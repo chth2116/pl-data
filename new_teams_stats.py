d62360f86121b061d09bf5ef9bfa5b80bca6f380
Christopher Thompson <chth2116@gmail.com>
2021-08-18 17:07:33 -0600

    test

--- new_teams_stats.py
+++ new_teams_stats.py
@@ -67,85 +67,85 @@ def update_player_list():
         #
         #
         # player_data = json.loads(requests.request("GET", url, headers=headers).text)['api']['players']
-#         # print(player_data)
-#         #
-#         # client = MongoClient('mongodb+srv://topher-thompson:Topher^0316@cluster-pldata.ezii8.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', ssl_cert_reqs=ssl.CERT_NONE)
-#         # db = client.teams
-#         # teams_collection = db["2022-2022"]
-#         #
-#         # personDocument = {
-#         #   "team": team_name,
-#         #   "players": player_data
-#         # }
-#         # teams_collection.insert_one(personDocument)
+        # print(player_data)
+        #
+        # client = MongoClient('mongodb+srv://topher-thompson:Topher^0316@cluster-pldata.ezii8.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', ssl_cert_reqs=ssl.CERT_NONE)
+        # db = client.teams
+        # teams_collection = db["2022-2022"]
+        #
+        # personDocument = {
+        #   "team": team_name,
+        #   "players": player_data
+        # }
+        # teams_collection.insert_one(personDocument)
+
+
+
+
+
+        #
+        # player_data_list = []
+        player_list = []
+        for player in player_data:
+            # url = "https://api-football-v1.p.rapidapi.com/v2/players/player/"+str(player['player_id'])+"/2021-2022"
+            # headers = {
+            #     'x-rapidapi-host': "api-football-v1.p.rapidapi.com",
+            #     'x-rapidapi-key': pl_data_api
+            #     }
+            # player_stats_response = json.loads(requests.request("GET", url, headers=headers).text)['api']['players']
+            # print(player_stats_response)
+            #
+            #
+            #
+            #
+            player_dict = {}
+            player_dict['name'] = player['player']['name']
+            player_dict['stats'] = player['player']['statistics']
+            player_list.append(player_dict)
+
+        client = MongoClient('mongodb+srv://topher-thompson:Topher^0316@cluster-pldata.ezii8.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', ssl_cert_reqs=ssl.CERT_NONE)
+        db = client.teams
+        teams_collection = db["2021-2022"]
+        personDocument = {
+          "team": team_name,
+          "player": player_list
+        }
+        teams_collection.insert_one(personDocument)
+        # #         player_data_dict1['Stats'] = player_stats_response
+        #         print(player_stats_response)
+        #     except:
+        #         print("error")
+        #         continue
+        #
+        #     player_data_dict = {}
+        #     player_data_dict['_id'] = player['player_id']
+        #     player_data_dict['Name'] = player['player_name']
+        #     player_data_dict['Firstname'] = player['firstname']
+        #     player_data_dict['Lastname'] = player['lastname']
+        #     player_data_dict['Position'] = player['position']
+        #     player_data_dict['Age'] = player_data_dict1
+        #
+        #     player_data_dict['Weight'] = player['weight']
+        #     player_data_dict['Nationality'] = player['nationality']
+        #     player_data_dict['Stats'] = player_stats_response
+        #
+        #
+        #
+        #     player_data_list.append(player_data_dict)
+        #
+        # client = MongoClient('mongodb+srv://topher-thompson:Topher^0316@cluster-pldata.ezii8.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', ssl_cert_reqs=ssl.CERT_NONE)
+        # db = client.teams
+        # teams_collection = db["2022-2022"]
+        #
+        # personDocument = {
+        #   "team": team_name,
+        #   "players": player_data_list
+        # }
+        # teams_collection.insert_one(personDocument)
 #
 #
 #
 #
-#
-#         #
-#         # player_data_list = []
-#         player_list = []
-#         for player in player_data:
-#             url = "https://api-football-v1.p.rapidapi.com/v2/players/player/"+str(player['player_id'])+"/2021-2022"
-#             headers = {
-#                 'x-rapidapi-host': "api-football-v1.p.rapidapi.com",
-#                 'x-rapidapi-key': pl_data_api
-#                 }
-#             player_stats_response = json.loads(requests.request("GET", url, headers=headers).text)['api']['players']
-#             print(player_stats_response)
-#
-#
-#
-#
-#             player_dict = {}
-#             player_dict['name'] = player['player_id']
-#             player_dict['stats'] = player_stats_response
-#             player_list.append(player_dict)
-#
-#         client = MongoClient('mongodb+srv://topher-thompson:Topher^0316@cluster-pldata.ezii8.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', ssl_cert_reqs=ssl.CERT_NONE)
-#         db = client.teams
-#         teams_collection = db["2021-2022"]
-#         personDocument = {
-#           "team": team_name,
-#           "player": player_list
-#         }
-#         teams_collection.insert_one(personDocument)
-#         # #         player_data_dict1['Stats'] = player_stats_response
-#         #         print(player_stats_response)
-#         #     except:
-#         #         print("error")
-#         #         continue
-#         #
-#         #     player_data_dict = {}
-#         #     player_data_dict['_id'] = player['player_id']
-#         #     player_data_dict['Name'] = player['player_name']
-#         #     player_data_dict['Firstname'] = player['firstname']
-#         #     player_data_dict['Lastname'] = player['lastname']
-#         #     player_data_dict['Position'] = player['position']
-#         #     player_data_dict['Age'] = player_data_dict1
-#         #
-#         #     player_data_dict['Weight'] = player['weight']
-#         #     player_data_dict['Nationality'] = player['nationality']
-#         #     player_data_dict['Stats'] = player_stats_response
-#         #
-#         #
-#         #
-#         #     player_data_list.append(player_data_dict)
-#         #
-#         # client = MongoClient('mongodb+srv://topher-thompson:Topher^0316@cluster-pldata.ezii8.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', ssl_cert_reqs=ssl.CERT_NONE)
-#         # db = client.teams
-#         # teams_collection = db["2022-2022"]
-#         #
-#         # personDocument = {
-#         #   "team": team_name,
-#         #   "players": player_data_list
-#         # }
-#         # teams_collection.insert_one(personDocument)
-# #
-# #
-# #
-# #
 def main():
     update_team_list()
     update_player_list()
